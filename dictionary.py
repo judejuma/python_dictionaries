4828e8398047fad17c72f72aaa7dce3ee5f91ca9
Jude <judejuma@gmail.com>
2018-11-15 13:07:40 +0300

    further practice on python dictionaries

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print (menu['Chicken Alfredo'])
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Guakamelo'] = 20.50
+menu['Fried Tuna'] = 15.30
+menu['Salmon'] = 20.10
+
+
+
+print ("There are " + str(len(menu)) + " items on the menu.")
+print (menu)
